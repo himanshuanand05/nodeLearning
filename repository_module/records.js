c9959a4134e5d001902568dd3f0cd228f36d5061
Himanshu Anand <himanshuanand05@gmail.com>
2022-03-18 07:59:57 +0530

    query execution

---
+++ repository_module/records.js
@@ -0,0 +1,52 @@
+import MongoClient from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export {
+    getRecordsByDateRangeAndCountSum
+}
+
+function getRecordsByDateRangeAndCountSum(data) {
+    return new Promise((resolve, reject)=>{
+        MongoClient.MongoClient.connect(process.env.CONNECTION_URI, function(err, db){
+            if(err) reject(err);
+            let dbo = db.db(process.env.DB_NAME);
+            let query = [
+                {
+                    $match: {
+                        $and: [
+                            { createdAt: { $gt: new Date(data.startDate) } },
+                            { createdAt: { $lt: new Date(data.endDate) } }
+                        ]
+                    }
+                },
+                {
+                    $addFields: {
+                        totalCount: { $sum: "$counts" }
+                    }
+                },
+                {
+                    $match: {
+                        $and: [
+                            { totalCount: { $gt: data.minCount} },
+                            { totalCount: { $lt: data.maxCount} }
+                        ]
+                    }
+                },
+                {
+                    $unset: ["value", "counts", "_id"]
+                }
+            ]
+
+            dbo.collection("records").aggregate(query).toArray(function (err, result) {
+                if (err) reject(err);
+                db.close();
+                resolve(result);
+            });
+        })
+    })
+}
+
+
+
--- server.js
+++ server.js
@@ -1,59 +1,19 @@
-const express = require("express")
-const app = express()
-var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require("body-parser");
+import express from "express"
+import bodyParser from "body-parser";
 
-const url = "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
-const dbName = "getir-case-study";
+import { getRecordsByDateRangeAndCountSum } from "./repository_module/records.js"
 
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-function insertData(params) {x
-    MongoClient.connect(url, (err, db)=>{
-        if (err) throw err;
-        let dbo = db.db(dbName);
-        //insert records in collection
-        dbo.collection("customers").insertMany(params, function (err, res) {
-            if (err) throw err;
-            console.log("Number of documents inserted: " + res.insertedCount);
-            db.close();
-        });
-    
-    })
-}
-function createDB() {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        let dbo = db.db(dbName);
-        
-        dbo.collection("records").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            db.close();
-            });
-    });
-}
-
-app.post('/', function (req, res) {
-    res.send('Hello World');
-    createDB()
-})
-
-app.post('/getData', function (req, res) {
-    
-})
-
-const app2 = express()
-
+const app = express();
 app.listen(3000, () => {
     console.log("Started server on 3000");
 });
 
-app2.listen(3002, () => {
-    console.log("Started server on 3002");
-});
+//get body data as json
+app.use(bodyParser.json());
 
-app2.get('/', function (req, res) {
-    res.send('Hello World from server 2');
+app.post('/', function (req, res, next) {
+    getRecordsByDateRangeAndCountSum(req.body)
+        .then(result=>res.json(result))
+        .catch(err=>next(err))
 })
+
